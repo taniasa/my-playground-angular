3ca56b77bdd207cd122d0fcfbbe90058d65c5a78
Tânia Salgado de Abreu <taniasgdo@hotmail.com>
2018-05-10 01:17:42 -0300

    update packages e primeiro form

--- my-playground-front/src/app/app-routing.module.ts
+++ my-playground-front/src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { AplicacaoComponent } from './aplicacao/aplicacao.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules} from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RelatorioComponent } from './relatorio/relatorio.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'aplicacao', component: AplicacaoComponent }
+  { path: 'aplicacao', component: AplicacaoComponent },
+  { path: 'relatorio', component: RelatorioComponent }
   ];
 
 
 @NgModule({
     exports: [ RouterModule ],
-    imports: [ RouterModule.forRoot(routes) ]
+    imports: [ RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules}) ]
   })
 
 export class AppRoutingModule { }
--- my-playground-front/src/app/app.module.ts
+++ my-playground-front/src/app/app.module.ts
@@ -1,13 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+
 
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyNavbarComponent } from './shared/my-navbar/my-navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import {  MatCardModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatFormFieldModule,
+    MatOption,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatSelect,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonToggleModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatTooltipModule
+  } from '@angular/material';
 
 import {StyleManager} from './shared/style-manager/style-manager';
 import {ThemeStorage} from './shared/theme-picker/theme-storage/theme-storage';
@@ -15,6 +59,7 @@ import {ThemePickerModule} from './shared/theme-picker/theme-picker';
 import { AplicacaoComponent } from './aplicacao/aplicacao.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { RelatorioComponent } from './relatorio/relatorio.component';
 
 
 @NgModule({
@@ -22,12 +67,15 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     MyNavbarComponent,
     AplicacaoComponent,
-    HomeComponent
+    HomeComponent,
+    RelatorioComponent
   ],
   imports: [
     BrowserModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     LayoutModule,
     MatToolbarModule,
     MatButtonModule,
@@ -35,12 +83,55 @@ import { HomeComponent } from './home/home.component';
     MatIconModule,
     MatListModule,
     MatCardModule,
+    MatFormFieldModule ,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+
     ThemePickerModule,
+
     AppRoutingModule,
+    FlexLayoutModule,
   ],
   providers: [
     StyleManager,
     ThemeStorage,
+    {provide: LOCALE_ID, useValue: 'pt-BR'},
+
   ],
   bootstrap: [AppComponent]
 })
